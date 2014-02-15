219b308983c65353e84f1d3ef1e37b5f65daa53c
Hatleking <jonasatre@hotmail.com>
2014-02-16 00:51:10 +0100

    Added existing files

---
+++ Test/src/Test.java
@@ -0,0 +1,28 @@
+import static com.hatleking.wer.math.MathUtil.*;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import com.hatleking.wer.string.StringUtil;
+import com.hatleking.wer.time.Timer;
+import com.sakratt.gus.Console;
+import com.sakratt.gus.OutputFrame;
+import com.sakratt.gus.Window;
+
+public class Test {
+	public static void main(String[] args) {
+
+	}
+
+	public static void printTimeglas(int høgde) {
+		assert isOdd(høgde);
+
+	}
+
+}
